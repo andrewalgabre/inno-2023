27bfa6014b755f4d9af0a3cc502bf60dff9737a4
patrick.richner <patrick.richner@bison-group.com>
2023-09-21 13:54:05 +0200

    Add Text-Field and Button

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-explore-container',
@@ -8,5 +10,14 @@ import { Component, Input } from '@angular/core';
 export class ExploreContainerComponent {
 
   @Input() name?: string;
+  @Input() textAreaInput?: string;
 
+  constructor() {
+  }
+  
+  sendMessageToDialogFlow(): void {
+    var text = this.textAreaInput;
+    console.log("Sending following text to dialog flow: " + text)
+  }
+  
 }
